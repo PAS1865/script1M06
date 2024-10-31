253158249d4b9c6a7b84596357829448440f2110
LexxGG <148267321+LexxGG@users.noreply.github.com>
2024-10-31 17:03:31 +0100

    Update gestio_backups.sh
    echos al catalan

--- scriptAvançades/gestio_backups.sh
+++ scriptAvançades/gestio_backups.sh
@@ -14,27 +14,27 @@ crear_backup(){
 	#Si la carpeta del nombre no existe creara la carpeta
 	if [ ! -d $nombackup ];then
 		mkdir $nombackup
-		echo "Se ha creado la carpeta backup" >> $filelog
- 		echo "Se ha creado la carpeta backup"
+		echo "S'ha creat la carpeta backup." >> $filelog
+ 		echo "S'ha creat la carpeta backup."
 	fi
 
 
 	#Seguidamente paso por pantalla un par de textos al usuario i le pido que rellene la variable carpeta seleccionada
 	echo -e "\n"
-	echo "A continuació el que farem es crear un backup d'una carpeta, aquest backup es creara amb una carpeta anomenada $nombackup"
-	read -p "Indica amb una ruta absoluta quina carpeta vols fer un backup: " carpetaSeleccionada
+	echo "A continuació, el que farem és crear una còpia de seguretat d'una carpeta. Aquesta còpia de seguretat es crearà en una carpeta anomenada $nombackup"
+	read -p "Indica amb una ruta absoluta quina carpeta vols fer una còpia de seguretat: " carpetaSeleccionada
 
 
 	#Si no existe la carpeta seleccionada imprime por pantalla que hay un error i no se crea el backup
 	if [ ! -d "$carpetaSeleccionada" ];then
-		echo -e "\033[31mERROR:\033[0m Aquesta carpeta no existeix, per tant no es fara cap backup." >> $filelog
-  		echo -e "\033[31mERROR:\033[0m Aquesta carpeta no existeix, per tant no es fara cap backup."
+		echo -e "\033[31mERROR:\033[0m Aquesta carpeta no existeix, per tant no es farà cap còpia de seguretat." >> $filelog
+  		echo -e "\033[31mERROR:\033[0m Aquesta carpeta no existeix, per tant no es farà cap còpia de seguretat."
 
 
 	#Si existe la carpeta pero detecta que la variable ls -A es nula significa que la carpeta esta vacia, por lo tanto no hace falta hacer backup
 	elif [ -z "$(find "$carpetaSeleccionada" -mindepth 1 -print -quit)" ];then
-		echo -e "\033[31mERROR:\033[0m Esta carpeta existe pero esta vacia, por tanto no se creara el backup." >> $filelog
-		echo -e "\033[31mERROR:\033[0m Esta carpeta existe pero esta vacia, por tanto no se creara el backup."
+		echo -e "\033[31mERROR:\033[0m Aquesta carpeta existeix, però està buida, per tant no es crearà la còpia de seguretat." >> $filelog
+		echo -e "\033[31mERROR:\033[0m Aquesta carpeta existeix, però està buida, per tant no es crearà la còpia de seguretat."
 
 	#Si no pasa nada de lo anterior el backup se crea i se pone dentro de la carpeta llamada backup
 	else
@@ -53,14 +53,14 @@ borrar_bkps(){
 
 	#Si no existe la carpeta seleccionada imprime por pantalla que hay un error i no se crea el backup
 	if [ ! -d "$nombackup" ];then
-	        echo -e "\033[31mERROR:\033[0m Aquesta carpeta no existeix, per tant no s'esborrara cap backup." >> $filelog
-	 	 echo -e "\033[31mERROR:\033[0m Aquesta carpeta no existeix, per tant no s'esborrara cap backup."
+	        echo -e "\033[31mERROR:\033[0m Aquesta carpeta no existeix, per tant no s'esborrarà cap còpia de seguretat." >> $filelog
+	 	 echo -e "\033[31mERROR:\033[0m Aquesta carpeta no existeix, per tant no s'esborrarà cap còpia de seguretat."
 
 
 	#Si existe la carpeta pero detecta que la variable ls -A es nula significa que la carpeta esta vacia, por lo tanto no hace falta hacer backup
 	elif [ -z "$(find "$nombackup" -mindepth 1 -print -quit)" ];then
-	        echo -e "\033[31mERROR:\033[0m Esta carpeta existe pero esta vacia, por tanto no se borrara el backup." >> $filelog
-	  	echo -e "\033[31mERROR:\033[0m Esta carpeta existe pero esta vacia, por tanto no se borrara el backup."
+	        echo -e "\033[31mERROR:\033[0m Aquesta carpeta existeix, però està buida, per tant no s'esborrarà la còpia de seguretat." >> $filelog
+	  	echo -e "\033[31mERROR:\033[0m Aquesta carpeta existeix, però està buida, per tant no s'esborrarà la còpia de seguretat."
 
 	else
 
@@ -82,8 +82,8 @@ borrar_bkps(){
 
 		# Verificar si la selección es un número válido
 		if ! [[ "$seleccion" =~ ^[0-9]+$ ]] || [ "$seleccion" -lt 1 ] || [ "$seleccion" -ge "$contador" ]; then
-		    echo -e "\033[31mERROR:\033[0m Selección inválida. Por favor, elige un número de la lista." >> $filelog
-      		    echo -e "\033[31mERROR:\033[0m Selección inválida. Por favor, elige un número de la lista."
+		    echo -e "\033[31mERROR:\033[0m Selecció invàlida. Si us plau, tria un número de la llista." >> $filelog
+      		    echo -e "\033[31mERROR:\033[0m Selecció invàlida. Si us plau, tria un número de la llista."
 		    exit 1
 		fi
 
@@ -91,14 +91,14 @@ borrar_bkps(){
 		archivo_a_eliminar=$(ls "$nombackup" | sed -n "${seleccion}p")
 
 		# Confirmar la eliminación
-		read -p "¿Estás seguro de que deseas eliminar el archivo '$archivo_a_eliminar'? (s/n): " confirmacion
+		read -p "¿Estàs segur que vols eliminar el fitxer '$archivo_a_eliminar'? (s/n): " confirmacion
 
 		if [[ "$confirmacion" =~ ^[sS]$ ]]; then
 		    rm "$nombackup/$archivo_a_eliminar"
-		    echo "El archivo '$archivo_a_eliminar' ha sido eliminado." >> $filelog
-      		    echo "El archivo '$archivo_a_eliminar' ha sido eliminado."
+		    echo "Fitxer '$archivo_a_eliminar' ha estat eliminat." >> $filelog
+      		    echo "Fitxer '$archivo_a_eliminar' ha estat eliminat."
 		else
-		    echo "Eliminación cancelada."
+		    echo "Eliminació cancel·lada."
 		fi
 	fi
 
@@ -109,14 +109,14 @@ extraer_backups(){
 
 	#Si no existe la carpeta seleccionada imprime por pantalla que hay un error i no se crea el backup
         if [ ! -d "$nombackup" ];then
-                echo -e "\033[31mERROR:\033[0m Aquesta carpeta no existeix, per tant no es fara cap backup." >> $filelog
-		echo -e "\033[31mERROR:\033[0m Aquesta carpeta no existeix, per tant no es fara cap backup."
+                echo -e "\033[31mERROR:\033[0m Aquesta carpeta no existeix, per tant no es farà cap còpia de seguretat." >> $filelog
+		echo -e "\033[31mERROR:\033[0m Aquesta carpeta no existeix, per tant no es farà cap còpia de seguretat."
 
 
         #Si existe la carpeta pero detecta que la variable ls -A es nula significa que la carpeta esta vacia, por lo tanto no hace falta hacer backup
         elif [ -z "$(find "$nombackup" -mindepth 1 -print -quit)" ];then
-                echo -e "\033[31mERROR:\033[0m Esta carpeta existe pero esta vacia, por tanto no se creara el backup." >> $filelog
-		echo -e "\033[31mERROR:\033[0m Esta carpeta existe pero esta vacia, por tanto no se creara el backup."
+                echo -e "\033[31mERROR:\033[0m Aquesta carpeta existeix, però està buida, per tant no es crearà la còpia de seguretat." >> $filelog
+		echo -e "\033[31mERROR:\033[0m Aquesta carpeta existeix, però està buida, per tant no es crearà la còpia de seguretat."
 
         else
 
@@ -124,7 +124,7 @@ extraer_backups(){
                 contador=1
 
                 # Usar un bucle para listar los archivos con números
-                echo "Archivos en $nombackup:"
+                echo "Fitxers en $nombackup:"
                 for archivo in "$nombackup"/*; do
                     # Verificar si el archivo existe (en caso de que no haya archivos)
                     if [ -e "$archivo" ]; then
@@ -134,39 +134,39 @@ extraer_backups(){
                 done
 
                 # Pedir al usuario que elija un archivo
-                read -p "Selecciona el número del backups que deseas extraer: " seleccion
+                read -p "Selecciona el número del còpia de seguretat que desitges extreure: " seleccion
 
                 # Verificar si la selección es un número válido
                 if ! [[ "$seleccion" =~ ^[0-9]+$ ]] || [ "$seleccion" -lt 1 ] || [ "$seleccion" -ge "$contador" ]; then
-                    echo -e "\033[31mERROR:\033[0m Selección inválida. Por favor, elige un número de la lista." >> $filelog
-		    echo -e "\033[31mERROR:\033[0m Selección inválida. Por favor, elige un número de la lista."
+                    echo -e "\033[31mERROR:\033[0m Selecció invàlida. Si us plau, tria un número de la llista." >> $filelog
+		    echo -e "\033[31mERROR:\033[0m Selecció invàlida. Si us plau, tria un número de la llista."
                     exit 1
                 fi
 
                 # Obtener el archivo correspondiente a la selección
                 archivo_a_extraer=$(ls "$nombackup" | sed -n "${seleccion}p")
 
-		read -p "Indica en que ruta quieres extraer el backup: " ruta_extraccion
+		read -p "Indica en quina ruta vols extreure la còpia de seguretat: " ruta_extraccion
 
 		if [ ! -d "$ruta_extraccion" ];then
-			echo "La ruta que has indicado no existia, pero se ha creado acontinuacion" >> $filelog
-  			echo "La ruta que has indicado no existia, pero se ha creado acontinuacion"
+			echo "La ruta que has indicat no existia, però s'ha creat a continuació" >> $filelog
+  			echo "La ruta que has indicat no existia, però s'ha creat a continuació"
 			mkdir -p "$ruta_extraccion"
 		fi
 
                 # Confirmar la eliminación
-		read -p "¿Estás seguro de que deseas extraer el archivo '$archivo_a_extraer'? (s/n): " confirmacion
+		read -p "¿Estàs segur que vols extreure el fitxer '$archivo_a_extraer'? (s/n): " confirmacion
 		if [[ "$confirmacion" =~ ^[sS]$ ]]; then
 		    tar -xvf "$nombackup/$archivo_a_extraer" -C "$ruta_extraccion"
-		    echo "El archivo '$archivo_a_extraer' ha sido extraído en $ruta_extraccion." >> $filelog
-       		    echo "El archivo '$archivo_a_extraer' ha sido extraído en $ruta_extraccion."
-		read -p "Indica si vols eliminar el backup que has extret anteriorment (s/n): " confirmacio2
+		    echo "Fitxer '$archivo_a_extraer' ha estat extret a $ruta_extraccion." >> $filelog
+       		    echo "Fitxer '$archivo_a_extraer' ha estat extret a $ruta_extraccion."
+		read -p "Indica si vols eliminar la còpia de seguretat que has extret anteriorment (s/n): " confirmacio2
 
-			echo "Se ha eliminado el backup"
+			echo "S'ha eliminat la còpia de seguretat"
 			rm "$nombackup/$archivo_a_extraer"
 
 		else
-		    echo "Extración cancelada."
+		    echo "Extracció cancel·lada."
 		fi
 
         fi
@@ -177,13 +177,13 @@ while true; do
 
 clear
 
-echo "BENVINGUTS AL SCRIPT DE GESTIO DE COPIES DE SEGURETAT"
+echo "BENVINGUTS AL SCRIPT DE GESTIÓ DE COPIES DE SEGURETAT"
 echo "1. Crear BackUps"
 echo "2. Eliminar BackUps"
-echo "3. Extraer Backups"
-echo "4. Salir"
+echo "3. Extreure Backups"
+echo "4. Sortir"
 echo -e "\n"
-read -p "Indica quina opcio vols triar: " opcio
+read -p "Indica quina opció vols triar: " opcio
 
 case "$opcio" in
 
